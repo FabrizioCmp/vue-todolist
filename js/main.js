a1ceb7be727582f2a16cb4f65fffc7c044c54aef
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-23 17:24:33 +0100

    Bonus 1&2

--- js/main.js
+++ js/main.js
@@ -5,31 +5,44 @@ createApp({
         return {
             tasks: [
                 {
-                    text: "task di prova",
-                    done: ""
+                    text: "Comprare il pane",
+                    done: false
                 },
                 {
-                    text: "task di prova2",
-                    done: ""
+                    text: "conquistare il mondo",
+                    done: false
                 }
             ],
             newTask: {
                 text: "",
-                done: ""
+                done: false
             }
 
         }
     },
     methods: {
+
+        //elimina il singolo task dall'array tasks
         removeTask(index) {
             this.tasks.splice(index, 1)
         },
-        onSubmit(){
+
+        // al submit del from crea un nuovo oggetto Task e lo aggiunge all'array taks
+        onSubmit() {
             this.tasks.push({
                 text: this.newTask.text,
-                done: ""
+                done: false
             })
             this.newTask.text = ""
+        },
+
+        // al click sul testo del singolo taks cambia il valore di done
+        clickTextTask(index) {
+            if (this.tasks[index].done === true) {
+                this.tasks[index].done = false;
+            } else {
+                this.tasks[index].done = true;
+            }
         }
     }
 }).mount("#app")
