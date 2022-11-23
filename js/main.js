fe6b7ec355a987dc1320ddf34f5b5b3b84cfc821
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-23 16:11:30 +0100

    Milestone 1&2

--- js/main.js
+++ js/main.js
@@ -7,11 +7,17 @@ createApp({
                 {
                     text: "task di prova",
                     done: false,
+                },
+                {
+                    text: "task di prova2",
+                    done: false,
                 }
             ]
         }
     },
     methods: {
-
+        removeTask(index) {
+            this.tasks.splice(index, 1)
+        }
     }
 }).mount("#app")
