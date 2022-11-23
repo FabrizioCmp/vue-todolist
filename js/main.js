ead8de4ef70c715386640644154da2f4c074fc6e
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-23 16:32:40 +0100

    MIlestone 3

--- js/main.js
+++ js/main.js
@@ -12,12 +12,24 @@ createApp({
                     text: "task di prova2",
                     done: ""
                 }
-            ]
+            ],
+            newTask: {
+                text: "",
+                done: ""
+            }
+
         }
     },
     methods: {
         removeTask(index) {
             this.tasks.splice(index, 1)
+        },
+        onSubmit(){
+            this.tasks.push({
+                text: this.newTask.text,
+                done: ""
+            })
+            this.newTask.text = ""
         }
     }
 }).mount("#app")
